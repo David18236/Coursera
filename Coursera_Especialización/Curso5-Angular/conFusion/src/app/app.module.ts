8320a8c52a9e9ea2946676b3e78cfdae6fe9d86c
David Ospina <dospinao@unal.edu.co>
2020-05-25 12:48:37 -0500

    Module Template Forms Part 2 Completed

--- Coursera_Especialización/Curso5-Angular/conFusion/src/app/app.module.ts
+++ Coursera_Especialización/Curso5-Angular/conFusion/src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 
@@ -48,9 +52,14 @@ import { LoginComponent } from './login/login.component';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    FlexLayoutModule,
-    AppRoutingModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
     MatDialogModule,
+    FlexLayoutModule,
+    FormsModule, 
+    AppRoutingModule,
+    
   ],
 
   providers: [
