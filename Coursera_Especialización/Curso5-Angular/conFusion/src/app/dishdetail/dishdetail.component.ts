e6762235e183cf5cc67c0b13f93ba58fb6c43bca
David Ospina <dospinao@unal.edu.co>
2020-05-28 16:12:42 -0500

    Module Saving Changes to Server Completed

--- Coursera_Especialización/Curso5-Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Coursera_Especialización/Curso5-Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
 
-import {Dish} from '../shared/dish';
+import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 
 import { Params, ActivatedRoute } from '@angular/router';
@@ -19,16 +19,17 @@ import { MatDialog, MatDialogRef } from '@angular/material';
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
-  
-  @ViewChild('fform') commentFormDirective; 
+
+  @ViewChild('fform') commentFormDirective;
   dish: Dish;
   dishIds: string[];
   prev: string;
   next: string;
   errMess: string;
+  dishcopy: Dish;
 
   commentForm: FormGroup;
-  comment:Comment;
+  comment: Comment;
 
   formErrors = {
     'author': '',
@@ -37,12 +38,12 @@ export class DishdetailComponent implements OnInit {
 
   validationMessages = {
     'author': {
-      'required':      'name is required.',
-      'minlength':     'name must be at least 2 characters long.',
-      'maxlength':     'name cannot be more than 25 characters long.'
+      'required': 'name is required.',
+      'minlength': 'name must be at least 2 characters long.',
+      'maxlength': 'name cannot be more than 25 characters long.'
     },
     'comment': {
-      'required':      'Comment is required.'
+      'required': 'Comment is required.'
     },
   };
 
@@ -53,72 +54,68 @@ export class DishdetailComponent implements OnInit {
     private fb: FormBuilder,
     @Inject('BaseURL') private BaseURL) { }
 
-    createForm() {
-      this.commentForm = this.fb.group({
-        author: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
-        rating: '5',
-        comment: ['', [Validators.required] ]
-      });
+  createForm() {
+    this.commentForm = this.fb.group({
+      author: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      rating: '5',
+      comment: ['', [Validators.required]]
+    });
 
-      this.commentForm.valueChanges
+    this.commentForm.valueChanges
       .subscribe(data => this.onValueChanged(data));
 
     this.onValueChanged(); // (re)set validation messages now
-    }
+  }
 
-    onValueChanged(data?: any) {
-      if (!this.commentForm) { return; }
-      const form = this.commentForm;
-      for (const field in this.formErrors) {
-        if (this.formErrors.hasOwnProperty(field)) {
-          // clear previous error message (if any)
-          this.formErrors[field] = '';
-          const control = form.get(field);
-          if (control && control.dirty && !control.valid) {
-            const messages = this.validationMessages[field];
-            for (const key in control.errors) {
-              if (control.errors.hasOwnProperty(key)) {
-                this.formErrors[field] += messages[key] + ' ';
-              }
+  onValueChanged(data?: any) {
+    if (!this.commentForm) { return; }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
             }
           }
         }
       }
     }
-  
-    onSubmit() {
-      this.comment = this.commentForm.value;
+  }
 
-      var d = new Date();
-      this.comment.date = d.toISOString();
-      
-      this.dish.comments.push(this.comment);
+  onSubmit() {
+    this.comment = this.commentForm.value;
+    var d = new Date();
+    this.comment.date = d.toISOString();
+    this.dishcopy.comments.push(this.comment);
+    this.dishservice.putDish(this.dishcopy).subscribe(dish => { this.dish = dish; this.dishcopy = dish; },
+      errmess => { this.dish = null; this.dishcopy = null; this.errMess = <any>errmess; });
+    console.log(this.comment);
+    this.commentForm.reset({
+      author: '',
+      comment: ''
+    });
+    this.commentFormDirective.resetForm({ rating: 5 });
+  }
 
-      console.log(this.dish.comments);
+  ngOnInit() {
+    this.createForm();
 
-      console.log(this.comment);
+    this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+    this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
+      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
+        errmess => this.errMess = <any>errmess);
+  }
 
-      this.commentForm.reset({
-        author: '',
-        comment: ''
-      });
-      this.commentFormDirective.resetForm({rating: 5});
-    }
-
-    ngOnInit() {
-      this.createForm();
-
-      this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
-      this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); },
-      errmess => this.errMess = <any>errmess);
-    }
-  
-    setPrevNext(dishId: string) {
-      const index = this.dishIds.indexOf(dishId);
-      this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
-      this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
-    }
+  setPrevNext(dishId: string) {
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
+  }
 
 
 
