475c27884e73ec6329ddef1954f328bdf6070cc1
David Ospina <dospinao@unal.edu.co>
2020-06-29 10:26:35 -0500

    Tarea 1 Java Completada

--- Coursera_Especialización/Curso7-JavaProgramming/Semana1/Tarea/PerimeterAssignmentRunner.java
+++ Coursera_Especialización/Curso7-JavaProgramming/Semana1/Tarea/PerimeterAssignmentRunner.java
@@ -26,7 +26,7 @@ public class PerimeterAssignmentRunner {
 
         // Iterar para contar la cantidad de puntos
         for (Point point : s.getPoints()) {
-            // Cad ves que itera suma un punto
+            // Cada ves que itera suma un punto
             numPoints = numPoints + 1;
         }
 
@@ -35,18 +35,58 @@ public class PerimeterAssignmentRunner {
     }
 
     public double getAverageLength(Shape s) {
-        // Put code here
-        return 0.0;
+        //Invoca la función que calcula la suma de las lingutudes
+        double sumLength = getPerimeter(s);
+        //Invoca la función que calcula el número de puntos de la figura
+        double numPoints = getNumPoints(s);
+
+        //Calcular el promedio de las longutudes de la figura
+        double averageLength = sumLength / numPoints;
+
+        //Retorna el promedio de las longitudes
+        return averageLength;
     }
 
     public double getLargestSide(Shape s) {
-        // Put code here
-        return 0.0;
+        //Inicializar la variable largestSide en 0
+        double largestSide = 0;
+        // Start with prevPt = the last point 
+        Point prevPt = s.getLastPoint();
+        
+        // For each point currPt in the shape,
+        for (Point currPt : s.getPoints()) {
+            // Find distance from prevPt point to currPt 
+            double currDist = prevPt.distance(currPt);
+
+            //Compara la anterior distancia con la actual
+            if (largestSide < currDist) {
+                //Si la actual es mayor a la anterior toma su lugar como el lado más largo
+                largestSide = currDist;
+            }
+            // Update prevPt to be currPt
+            prevPt = currPt;
+        }        
+        
+        return largestSide;
     }
 
     public double getLargestX(Shape s) {
-        // Put code here
-        return 0.0;
+        //Inicializo la variable largestX
+        double largestX = 0;
+
+        //Para cada X de cada punto
+        for (Point currPt : s.getPoints()){
+            //
+            double currX = currPt.getX();
+
+            //
+            if (largestX < currX){
+                //toma su lugar como el mayor X
+                largestX =  currX;
+            }
+        }
+
+        return largestX;
     }
 
     public double getLargestPerimeterMultipleFiles() {
@@ -65,7 +105,14 @@ public class PerimeterAssignmentRunner {
         Shape s = new Shape(fr);
         double length = getPerimeter(s);
         System.out.println("perimeter = " + length);
-        
+        int numPoints = getNumPoints(s);
+        System.out.println("numPoints = " + numPoints);
+        double averageLength = getAverageLength(s);
+        System.out.println("averageLength = " + averageLength);
+        double largestSide = getLargestSide(s);
+        System.out.println("largestSide = " + largestSide);
+        double largestX = getLargestX(s);
+        System.out.println("largestX = " + largestX);
     }
     
     public void testPerimeterMultipleFiles() {
@@ -74,13 +121,6 @@ public class PerimeterAssignmentRunner {
 
     public void testFileWithLargestPerimeter() {
         // Put code here
-    }
-
-    public void simpletest() {
-        FileResource fr = new FileResource();
-        Shape s = new Shape(fr);
-        int numPoints = getNumPoints(s);
-        System.out.println("numPoints= " + numPoints);
     }
 
     // This method creates a triangle that you can use to test your other methods
@@ -106,7 +146,6 @@ public class PerimeterAssignmentRunner {
 
     public static void main (String[] args) {
         PerimeterAssignmentRunner pr = new PerimeterAssignmentRunner();
-        //pr.testPerimeter();
-        pr.simpletest();
+        pr.testPerimeter();
     }
 }
