ebcebb7e2749177614fcfd8607686ae645145b87
David Ospina <dospinao@unal.edu.co>
2020-07-09 14:03:35 -0500

    Inicio Course PHP

---
+++ Coursera/Curso7-JavaProgramming/Semana2/FileResources/WhileLoop/AllCodons.java
@@ -0,0 +1,22 @@
+public class AllCodons {
+    public int findStopCodon(String dnaStr, int startIndex, String stopCodon) {
+        int currIndex = dnaStr.indexOf(stopCodon, startIndex + 3);
+
+        while (currIndex != -1) {
+            int diff = currIndex - startIndex;
+            if(diff % 3 == 0) {
+                return currIndex;
+            }
+
+            else {
+                currIndex = dnaStr.indexOf(stopCodon, currIndex + 1);
+            }
+        }
+
+        return dnaStr.length();
+    }
+
+    public String findGen(String dna) {
+
+    }
+}
